e9fb3b7d97a219837f0ef3b88f211f73591e93a0
Aman O <amanclct@gmail.com>
2025-01-06 15:28:23 +0000

    Add key mapping for special keys like arrow, home, end, delete

--- src/main/java/org/texteditor/Main.java
+++ src/main/java/org/texteditor/Main.java
@@ -10,6 +10,17 @@ import java.util.Arrays;
 
 public class Main {
     private static LibC.Termios defaultAttributes;
+
+    private static final int ARROW_UP = 1000;
+    private static final int ARROW_DOWN = 1001;
+    private static final int ARROW_RIGHT = 1002;
+    private static final int ARROW_LEFT = 1003;
+    private static final int PAGE_UP = 1004;
+    private static final int PAGE_DOWN = 1005;
+    private static final int HOME = 1006;
+    private static final int END = 1007;
+    private static final int DEL = 1008;
+
     private static int rows = 10;
     private static int columns = 10;
 
@@ -26,12 +37,47 @@ public class Main {
 
     private static void init() {
         LibC.WinSize winSize = getWindowSize();
+
         columns = winSize.ws_col;
         rows = winSize.ws_row;
     }
 
     private static int readKey() throws IOException {
-        return System.in.read();
+        int key = System.in.read();
+        if (key != '\033') {
+            return key;
+        }
+
+        int secondKey = System.in.read();
+        if (secondKey != '[' && secondKey != 'O') {
+            return secondKey;
+        }
+
+        int thirdKey = System.in.read();
+
+        return switch (thirdKey) {
+            case 'A' -> ARROW_UP;
+            case 'B' -> ARROW_DOWN;
+            case 'C' -> ARROW_RIGHT;
+            case 'D' -> ARROW_LEFT;
+            case 'H' -> HOME;
+            case 'F' -> END;
+            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
+                int fourthKey = System.in.read();
+                if (fourthKey != '~') {
+                    yield fourthKey;
+                }
+                switch (thirdKey) {
+                    case '1', '7' -> { yield HOME; }
+                    case '3' -> { yield DEL; }
+                    case '4', '8' -> { yield END; }
+                    case '5' -> { yield PAGE_UP; }
+                    case '6' -> { yield PAGE_DOWN; }
+                    default -> { yield fourthKey; }
+                }
+            }
+            default -> thirdKey;
+        };
     }
 
     private static void handleKeyRead(int key) {
@@ -39,9 +85,9 @@ public class Main {
             resetAttributes();
             cleanup();
             System.exit(0);
+        } else {
+            System.out.print((char) key + " -> " + key + "\r\n");
         }
-
-        System.out.print((char) key);
     }
 
     private static void enableRawMode() {
@@ -65,21 +111,32 @@ public class Main {
     }
 
     private static void refreshScreen() {
-        System.out.print("\033[2J");
-        System.out.print("\033[H");
+        StringBuilder builder = new StringBuilder();
 
-        for (int i = 0; i < rows - 1; i++) {
-            System.out.print("~\r\n");
-        }
+        builder.append("\033[2J");
+        builder.append("\033[H");
+
+        builder.append("~\r\n".repeat(Math.max(0, rows - 1)));
 
         String message = "Text Editor v0.1";
+        builder.append("\033[7m")
+                .append(message)
+                .append(" ".repeat(Math.max(0, columns - message.length())))
+                .append("\033[0m");
 
-        System.out.print("\033[7m" + message + " ".repeat(Math.max(0, columns - message.length())) + "\033[0m");
+        builder.append("\033[H");
+        System.out.print(builder);
     }
 
     private static LibC.WinSize getWindowSize() {
-        LibC.WinSize winSize = new LibC.WinSize();
-        LibC.INSTANCE.ioctl(LibC.SYSTEM_OUT_FD, LibC.TI0CGWINSZ, winSize);
+        final LibC.WinSize winSize = new LibC.WinSize();
+        final int rc = LibC.INSTANCE.ioctl(LibC.SYSTEM_OUT_FD, LibC.TI0CGWINSZ, winSize);
+
+        if (rc != 0) {
+            System.err.println("Failed to get ioctl. Error code: " + rc);
+            System.exit(1);
+        }
+
         return winSize;
     }
 
@@ -129,7 +186,7 @@ interface LibC extends Library {
 
     @Structure.FieldOrder(value = {"ws_row", "ws_col", "ws_xpixel", "ws_ypixel"})
     class WinSize extends Structure {
-        public int ws_row, ws_col, ws_xpixel, ws_ypixel;
+        public short ws_row, ws_col, ws_xpixel, ws_ypixel;
 
         @Override
         public String toString() {
